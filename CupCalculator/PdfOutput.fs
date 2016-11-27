28e4fb643b158359b49d256f00d72b36bb3d1793
bogma <bogma@users.noreply.github.com>
2016-11-27 07:57:52 +0100

    pdf output improvments

--- CupCalculator/PdfOutput.fs
+++ CupCalculator/PdfOutput.fs
@@ -19,62 +19,54 @@ let setStyles (document:Document) =
     // Heading1 to Heading9 are predefined styles with an outline level -> creates bookmark
     let style = document.Styles.[StyleNames.Heading1]
     style.Font.Name <- "Segoe UI Light"
-    style.Font.Size <- new Unit(16.0)
+    style.Font.Size <- new Unit(12.0)
     style.Font.Bold <- true
     style.Font.Color <- Colors.DarkBlue
-    style.ParagraphFormat.PageBreakBefore <- true
-    style.ParagraphFormat.SpaceAfter <- new Unit(6.0)
-    style.ParagraphFormat.KeepWithNext <- true
-
-    let style = document.Styles.[StyleNames.Heading2]
-    style.Font.Size <- new Unit(14.0)
-    style.Font.Bold <- true
     style.ParagraphFormat.PageBreakBefore <- false
     style.ParagraphFormat.SpaceBefore <- new Unit(6.0)
-    style.ParagraphFormat.SpaceAfter <- new Unit(6.0)
+    style.ParagraphFormat.SpaceAfter <- new Unit(4.0)
+    style.ParagraphFormat.KeepWithNext <- true
 
     let style = document.Styles.[StyleNames.Header]
-    style.ParagraphFormat.AddTabStop(new Unit(16.0, UnitType.Centimeter), TabAlignment.Right) |> ignore
+    style.ParagraphFormat.AddTabStop(new Unit(16.0, UnitType.Centimeter), TabAlignment.Center) |> ignore
 
     let style = document.Styles.[StyleNames.Footer]
-    style.ParagraphFormat.AddTabStop(new Unit(8.0, UnitType.Centimeter), TabAlignment.Right) |> ignore
+    style.ParagraphFormat.AddTabStop(new Unit(8.0, UnitType.Centimeter), TabAlignment.Center) |> ignore
     0
 
-let head (document:Document) =
+let defineContentSection (document:Document) =
     let section = document.AddSection()
+    section.PageSetup.OddAndEvenPagesHeaderFooter <- false
+    section.PageSetup.StartingNumber <- 1
+
+//    section.PageSetup.TopMargin <- new Unit(15.0, UnitType.Millimeter)
+//    section.PageSetup.BottomMargin <- new Unit(10.0, UnitType.Millimeter)
+//    section.PageSetup.LeftMargin <- new Unit(5.0, UnitType.Millimeter)
+//    section.PageSetup.RightMargin <- new Unit(5.0, UnitType.Millimeter)
+
     match Config.Output.Pdf.Orientation with
     | "Portrait" -> section.PageSetup.Orientation <- Orientation.Portrait
     | _ -> section.PageSetup.Orientation <- Orientation.Landscape
 
-    let paragraph = section.AddParagraph()
-    paragraph.Format.SpaceAfter <- new Unit(3.0, UnitType.Centimeter)
+    let header = section.Headers.Primary
 
-    //let image = section.AddImage("../../../../assets/images/Logo landscape.png")
-    //image.Width <- new Unit(10.0, UnitType.Centimeter)
-    
-    let paragraph = section.AddParagraph("Rangliste - " + Config.Cup.Name)
-    paragraph.Format.Font.Size <- new Unit(16.0)
-    paragraph.Format.Font.Color <- Colors.DarkRed
+    let formattedText = new FormattedText()
 
-    let paragraph = section.AddParagraph("Gewertet werden die " + Config.Cup.TakeBest.ToString() + " besten Ergebnisse von " + Config.Cup.NumberOfEvents.ToString() + ".");
-    paragraph.Format.Font.Size <- new Unit(12.0)
-    paragraph.Format.Font.Color <- Colors.DarkRed;
+    let paragraph = header.AddParagraph()
+    let formattedText = paragraph.AddFormattedText("Rangliste - " + Config.Cup.Name)
+    formattedText.Size <- new Unit(14.0)
+    formattedText.Color <- Colors.DarkRed
 
-    let paragraph = section.AddParagraph("Erstellt am: ")
-    paragraph.AddDateField() |> ignore
-    0
+    let formattedText = paragraph.AddFormattedText(" (Gewertet werden die " + Config.Cup.TakeBest.ToString() + " besten Ergebnisse von " + Config.Cup.NumberOfEvents.ToString() + ".)");
+    formattedText.Size <- new Unit(10.0)
+    formattedText.Color <- Colors.Black;
 
-let defineFooter (document:Document) =
-    let section = document.AddSection()
-    let paragraph = section.AddParagraph()
+    let footer = section.Footers.Primary  
 
-    let footer = "(c) " + System.DateTime.Now.Year.ToString() + " by solv.at | Daten: ANNE / oefol.at und der veranstaltende Verein webmaster@solv.at Erstellt: " + System.DateTime.Now.ToString("R")
+    let footerTxt = "(c) " + System.DateTime.Now.Year.ToString() + " by solv.at | Daten: ANNE / oefol.at und der veranstaltende Verein webmaster@solv.at Erstellt: " + System.DateTime.Now.ToString("R")
+    let paragraph = footer.AddParagraph()
 
-    let paragraph = section.Footers.Primary.AddParagraph();
-    paragraph.AddText(footer) |> ignore
-    paragraph.Format.Font.Size <- new Unit(4.0)
-    paragraph.Format.Alignment <- ParagraphAlignment.Center
-    0
+    paragraph.AddText(footerTxt) |> ignore
 
 let printSingleDetailedResult eventResult =
     let formattedText = new FormattedText()
@@ -109,7 +101,7 @@ let printDetailedResultCells (results:seq<EventResult>) (row:Row) =
                                 paragraph.Add(txt) |> ignore)
 
 let addTable (document:Document) classHeader (classResult : seq<CupResult>) =
-    document.LastSection.AddParagraph(classHeader, StyleNames.Heading2) |> ignore
+    document.LastSection.AddParagraph(classHeader, StyleNames.Heading1) |> ignore
 
     let table = new Table()
     table.Borders.Width <- new Unit(0.75)
@@ -169,8 +161,7 @@ let buildResultPdf catResults (outputFile:string) =
     doc.Info.Title <- Config.Cup.Name
     
     setStyles doc |> ignore
-    head doc |> ignore
-    defineFooter doc |> ignore
+    defineContentSection doc |> ignore
 
     let classCfg = Config.Classes |> Array.toList
     let catRes =
